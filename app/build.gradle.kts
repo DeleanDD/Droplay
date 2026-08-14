b395595ce6d4f57668ff7d7cc15b27b05fa4052c
Delean Lenardão <delean@digitaldrop.com.br>
2026-08-14 19:10:52 -0300

    feat(branding): atualiza ícone do Android TV

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.droplay.tv"
         minSdk = 23
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.2.9"
+        versionCode = 13
+        versionName = "1.2.10"
     }
 
     buildFeatures {
